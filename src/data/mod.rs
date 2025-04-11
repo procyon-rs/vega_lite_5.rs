7f43bf2c2bcaf19e50de6fbf74070307950f3bc7
David Bernard <david.bernard.31@gmail.com>
2025-04-11 17:34:25 +0200

    🚨 fix lint, format,...

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -3,25 +3,15 @@ pub use base_data::*;
 
 #[cfg(feature = "nalgebra")]
 mod nalgebra_data;
-#[cfg(feature = "nalgebra")]
-pub use nalgebra_data::*;
 
 #[cfg(feature = "rulinalg")]
 mod rulinalg_data;
-#[cfg(feature = "rulinalg")]
-pub use rulinalg_data::*;
 
 #[cfg(feature = "ndarray")]
 mod ndarray_data;
-#[cfg(feature = "ndarray")]
-pub use ndarray_data::*;
 
 #[cfg(feature = "csv")]
 mod csv_data;
-#[cfg(feature = "csv")]
-pub use csv_data::*;
 
 #[cfg(feature = "polars")]
 mod polars_data;
-#[cfg(feature = "polars")]
-pub use polars_data::*;
